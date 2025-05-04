b9468f9eaa7beae2ea805c98b38682104e8d2ce8
DaniDaGame <Danielevi71007@gmail.com>
2025-05-04 08:44:03 +0300

    try1

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,33 +1,37 @@
-'use client'
+'use client';
 
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React from 'react';
-import { useState } from 'react';
-import MeetingSetup from '@/components/MeetingSetup'
-import MeetingRoom from '@/components/MeetingRoom'
+import React, { useState } from 'react';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
 import { useGetCallById } from '@/hooks/useGetCallById';
 import Loader from '@/components/Loader';
 
-const Meeting = ({ params: {id} }: { params: { id: string }}) => {
-  const {user, isLoaded} = useUser();
-  const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const {call, isCallLoading} = useGetCallById(id);
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-  if(!isLoaded || isCallLoading) return <Loader />
+export default function Page({ params: { id } }: PageProps) {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
   return (
-    <main className='h-screen w-full'>
+    <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete = {setIsSetupComplete}/> 
-          ):(
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
             <MeetingRoom />
           )}
         </StreamTheme>
       </StreamCall>
     </main>
-  )
+  );
 }
-
-export default Meeting
