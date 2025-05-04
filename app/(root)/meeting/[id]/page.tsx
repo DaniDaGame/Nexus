28a0c6c2a8f35cb4743adb357616f422560ef0b7
DaniDaGame <Danielevi71007@gmail.com>
2025-05-04 03:14:23 +0300

    try1

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -2,52 +2,32 @@
 
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React from 'react';
 import { useState } from 'react';
-import MeetingSetup from '@/components/MeetingSetup';
-import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup'
+import MeetingRoom from '@/components/MeetingRoom'
 import { useGetCallById } from '@/hooks/useGetCallById';
 import Loader from '@/components/Loader';
 
-interface MeetingPageProps {
-  params: { id: string };
-}
-
-const MeetingPage = ({ params: { id } }: MeetingPageProps) => {
-  const { user, isLoaded } = useUser();
+const Meeting = ({ params: {id} }: { params: { id: string }}) => {
+  const {user, isLoaded} = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading } = useGetCallById(id);
+  const {call, isCallLoading} = useGetCallById(id);
 
-  if (!isLoaded || isCallLoading) return <Loader />;
-
-  if (!call) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <p>Meeting not found or has ended</p>
-      </div>
-    );
-  }
-
-  if (!user) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <p>Please sign in to join the meeting</p>
-      </div>
-    );
-  }
-
+  if(!isLoaded || isCallLoading) return <Loader />
   return (
-    <main className="h-screen w-full">
+    <main className='h-screen w-full'>
       <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-          ) : (
+            <MeetingSetup setIsSetupComplete = {setIsSetupComplete}/> 
+          ):(
             <MeetingRoom />
           )}
         </StreamTheme>
       </StreamCall>
     </main>
-  );
-};
+  )
+}
 
-export default MeetingPage;
+export default Meeting
