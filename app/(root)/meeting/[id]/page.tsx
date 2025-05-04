92c98e286a88f71b5892b356404ea495166ad3ff
DaniDaGame <Danielevi71007@gmail.com>
2025-05-04 22:51:04 +0300

    try1

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,31 +1,5 @@
-'use client';
+import MeetingClient from "@/components/MeetingClient";
 
-import { useUser } from '@clerk/nextjs';
-import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react';
-import MeetingSetup from '@/components/MeetingSetup';
-import MeetingRoom from '@/components/MeetingRoom';
-import { useGetCallById } from '@/hooks/useGetCallById';
-import Loader from '@/components/Loader';
-
-export default function Page({ params }: { params: { id: string } }) {
-  const { user, isLoaded } = useUser();
-  const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading } = useGetCallById(params.id);
-
-  if (!isLoaded || isCallLoading) return <Loader />;
-
-  return (
-    <main className="h-screen w-full">
-      <StreamCall call={call}>
-        <StreamTheme>
-          {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-          ) : (
-            <MeetingRoom />
-          )}
-        </StreamTheme>
-      </StreamCall>
-    </main>
-  );
+export default async function Page({ params }: { params: { id: string } }) {
+  return <MeetingClient id={params.id} />;
 }
